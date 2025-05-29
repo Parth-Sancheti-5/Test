f75903b61f3015c49f17f875f3c14e469114983e
Parth Sancheti <parthsancheti5@gmail.com>
2025-05-29 09:54:59 +0530

    Add files via upload

---
+++ post-fs-data.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/extension/com.oplus.app-features.xml /my_product/etc/extension/com.oplus.app-features.xml
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/extension/com.oplus.oplus-feature.xml /my_product/etc/extension/com.oplus.oplus-feature.xml
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/extension/feature_extension_com.android.launcher.xml my_product/etc/extension/feature_extension_com.android.launcher.xml
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/extension/realme_product_rom_extend_feature_23682.xml /my_product/etc/extension/realme_product_rom_extend_feature_23682.xml
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/permissions/oplus.feature.android.xml /my_product/etc/permissions/oplus.feature.android.xml
+mount -o bind /data/adb/modules/Blur_animation/my_product/etc/vibrator/ /my_product/etc/vibrator/
+mount -o bind /data/adb/modules/Blur_animation/my_product/overlay/ /my_product/overlay/
+mount -o bind /data/adb/modules/Blur_animation/system/system/ /system/system/
+mount -o bind /data/adb/modules/Blur_animation/system/system/app/ /system/system/app/
+mount -o bind /data/adb/modules/Blur_animation/system/system/lib/ /system/system/lib/
+mount -o bind /data/adb/modules/Blur_animation/system/system/lib64/ /system/system/lib64/
+mount -o bind /data/adb/modules/Blur_animation/system/system/framework/ /system/system/framework/
+mount -o bind /data/adb/modules/Blur_animation/system/system/etc/ /system/system/etc/
